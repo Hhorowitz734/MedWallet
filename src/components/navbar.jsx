9cdd212c615747871b737b5d712d1a3a3674accd
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-02-03 17:36:44 -0600

    completed responsive navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,20 @@
+import { FiMenu } from 'react-icons/fi'
+import { useState } from 'react'
+
 function Navbar(){
+    const [open, setOpen] = useState(false);
     return (
-        <header class="border-b border-sky-500 py-2">
-            <div class = 'flex items-center justify-between max-w-7xl mx-auto'>
-                <h3 class="">Lrgo here</h3>
-                <nav class="flex items-center w-auto">
-                    <ul class="text-base px-5 py-2 font-semibold text-white flex justify-between">
-                        <li><a class = "text-white hover:text-sky-500" href = '#'>Home</a></li>
-                        <li><a class = "text-white hover:text-sky-500" href = '#'>Our Work</a></li>
-                        <li><a class = "text-white hover:text-sky-500" href = '#'>Our Mission</a></li>
-                        <li><a class = "text-white hover:text-sky-500" href = '#'>Contact Us</a></li>
+        <header class="border-b border-sky-500 py-2 w-full">
+            <div class = 'flex items-center justify-between xl:max-w-7xl w-full mx-auto px-[2%] flex-wrap'>
+                <h3 class="">Logo here</h3>
+                <FiMenu class="lg:hidden block h-6 w-6 cursor-pointer" onClick = {() => setOpen(!open)}/>
+                <nav class={`${open ? 'flex' : 'hidden'} items-center lg:w-auto lg:flex w-full`}>
+                    <ul class="text-base px-5 py-2 font-semibold text-white lg:flex lg:justify-between w-full select-none">
+                        <li class = 'text-center'><a class = "text-white hover:text-sky-500 lg:px-4 h-10 w-full block py-2" href = '#'>Home</a></li>
+                        <li class = 'text-center'><a class = "text-white hover:text-sky-500 lg:px-4 h-10 w-full block py-2" href = '#'>Our Work</a></li>
+                        <li class = 'text-center'><a class = "text-white hover:text-sky-500 lg:px-4 h-10 w-full block py-2" href = '#'>Our Mission</a></li>
+                        <li class = 'text-center'><a class = "text-white hover:text-sky-500 lg:px-4 h-10 w-full block py-2" href = '#'>Contact Us</a></li>
+                        <li class = 'lg:pl-4 text-center'><a class = "text-white px-4 bg-blue-700 rounded-md h-10 w-15 py-2 block" href = '#'>Login</a></li>
                     </ul>
                 </nav>
             </div>
