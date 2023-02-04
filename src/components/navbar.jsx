3e9d04a51ba99531346275d723a64d7cae1715b3
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-02-04 10:50:56 -0600

    made changes to navbar and created navigation for login page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar(){
     return (
         <header class="border-b border-sky-500 py-2 w-full">
             <div class = 'flex items-center justify-between xl:max-w-7xl w-full mx-auto px-[2%] flex-wrap'>
-                <h3 class="">Logo here</h3>
+                <h3 class="">MedWallet</h3>
                 <FiMenu class="lg:hidden block h-6 w-6 cursor-pointer" onClick = {() => setOpen(!open)}/>
                 <nav class={`${open ? 'flex' : 'hidden'} items-center lg:w-auto lg:flex w-full`}>
                     <ul class="text-base px-5 py-2 font-semibold text-white lg:flex lg:justify-between w-full select-none">
@@ -14,7 +14,7 @@ function Navbar(){
                         <li class = 'text-center'><a class = "text-white hover:text-sky-500 lg:px-4 h-10 w-full block py-2" href = '#'>Our Work</a></li>
                         <li class = 'text-center'><a class = "text-white hover:text-sky-500 lg:px-4 h-10 w-full block py-2" href = '#'>Our Mission</a></li>
                         <li class = 'text-center'><a class = "text-white hover:text-sky-500 lg:px-4 h-10 w-full block py-2" href = '#'>Contact Us</a></li>
-                        <li class = 'lg:pl-4 text-center'><a class = "text-white px-4 bg-blue-700 rounded-md h-10 w-15 py-2 block" href = '#'>Login</a></li>
+                        <li class = 'lg:pl-4 text-center'><a class = "text-white px-4 bg-blue-700 hover:bg-blue-900 hover:text-dimWhite transition duration-500 rounded-md h-10 w-15 py-2 block" href = '#'>Login</a></li>
                     </ul>
                 </nav>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/loginpage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
